ffccb2a35403090094a28c215802a8bf47082356
Daniel Zaar <daniel.zaar@beijerelectronics.com>
2022-06-28 15:53:03 +0200

    Add first user input screen scetch

---
+++ HackathonX/UserInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace HackathonX.UI
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class UserInput : Window
+    {
+        public UserInput()
+        {
+            InitializeComponent();
+        }
+        private void StartPlaying_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("..Here we go");
+        }
+
+    }
+}
